51450d5a406634148a7a87b2023642972f8b3ff7
Augusto Pedraza <augusto.pedraza08@gmail.com>
2014-03-13 09:13:30 -0300

    Updated to get area requests

--- main.rb
+++ main.rb
@@ -13,17 +13,28 @@ class OpportunitiesSeeker
   end
 
   def online_requests
-    opportunities = []
+    get_requests('Main_div_onlineopp', 'Main_lb_onlineopppgNext')
+  end
 
-    opportunities << extract_opportunities_from_table
-    opportunities << extract_opportunities_from_table while navigate_to_next_page
-
-    opportunities.flatten!
+  def area_requests
+    get_requests('Main_div_sel', 'Main_lb_SelpgNext')
   end
 
   private
-    def extract_opportunities_from_table
-      @driver.find_element(:id, 'Main_div_onlineopp').find_elements(:tag_name, 'tr')
+    def get_requests(table_container_id, next_page_id)
+      @page_number = 0
+      @last_id = 0
+
+      opportunities = []
+
+      opportunities << extract_opportunities_from_table(table_container_id)
+      opportunities << extract_opportunities_from_table(table_container_id) while navigate_to_next_page(next_page_id)
+
+      opportunities.flatten!
+    end
+
+    def extract_opportunities_from_table(table_container_id)
+      @driver.find_element(:id, table_container_id).find_elements(:tag_name, 'tr')
         .map { |tr| tr.find_elements(:tag_name, 'td') }
         .map do |user_td, subject_td, date_td|
           extract_opportunity_from_table_row(user_td, subject_td, date_td)
@@ -32,6 +43,8 @@ class OpportunitiesSeeker
 
     def extract_opportunity_from_table_row(user_td, subject_td, date_td)
       opportunity = {}
+      @last_id = @last_id + 1
+      opportunity[:id]           = @last_id
       opportunity[:username]     = user_td.text
       opportunity[:user_profile] = user_td.find_element(:tag_name, 'a').attribute 'href'
       opportunity[:subject]      = subject_td.text
@@ -40,16 +53,17 @@ class OpportunitiesSeeker
       opportunity
     end
 
-    def navigate_to_next_page
+    def navigate_to_next_page(next_page_id)
       begin
-        while next_page_el = @driver.find_element(:id, 'Main_lb_onlineopppgNext')
+        if next_page_el = @driver.find_element(:id, next_page_id)
           next_page_el.click
-
+          @page_number = @page_number + 1
+          puts "Getting request from page n° #{@page_number}..."
           true #return
         end
       rescue Selenium::WebDriver::Error::NoSuchElementError
         puts "="*50
-        puts "ERROR!!!"
+        puts "No more pages!!!"
         puts "="*50
 
         false #return
@@ -70,6 +84,26 @@ login(driver)
 
 opportunities_seeker = OpportunitiesSeeker.new driver
 
-open("requests.txt", "w") { |io| io.write JSON.pretty_generate(opportunities_seeker.online_requests) }
+
+puts "="*50
+puts "Getting online opportunities..."
+puts "="*50
+
+opportunities = opportunities_seeker.online_requests
+puts "#{opportunities.count} online opportunities got."
+open("online_requests.txt", "w") { |io| io.write JSON.pretty_generate(opportunities) }
+
+
+#========================================
+
+
+puts "="*50
+puts "Getting area opportunities..."
+puts "="*50
+
+opportunities = opportunities_seeker.area_requests
+puts "#{opportunities.count} area opportunities got."
+
+open("area_requests.txt", "w") { |io| io.write JSON.pretty_generate(opportunities) }
 
 driver.quit
