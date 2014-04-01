7de3536d10171fc8562e1fbea05c75a31522a012
Augusto Pedraza <augusto.pedraza08@gmail.com>
2014-04-01 08:16:33 -0300

    The SeleniumDirectives#setup_address method was fully implemented

--- lib/selenium_directives.rb
+++ lib/selenium_directives.rb
@@ -1,8 +1,14 @@
 class SeleniumDirectives
-  LOGIN_URL             = "https://www.tutorselect.com/login"
-  USERNAME_ID           = 'Main_LoginUser_UserName'
-  PASSWORD_ID           = 'Main_LoginUser_Password'
-  WELCOME_MESSAGE_CLASS = 'username'
+  LOGIN_URL                 = "https://www.tutorselect.com/login"
+  USERNAME_ID               = 'Main_LoginUser_UserName'
+  PASSWORD_ID               = 'Main_LoginUser_Password'
+  WELCOME_MESSAGE_CLASS     = 'username'
+
+  EDIT_ADDRESS_PROFILE_ID   = 'Main_lb_detaddr_edit'
+  CITY_TEXTBOX_ID           = 'Main_tb_addrCity'
+  ZIPCODE_TEXTBOX_ID        = 'Main_tb_addrZip'
+  STATES_SELECT_ID          = 'Main_ddl_States'
+  ADDRESS_PROFILE_SUBMIT_ID = 'Main_lb_addrSave'
 
   def initialize(driver)
     @driver = driver
@@ -33,6 +39,23 @@ class SeleniumDirectives
 
   def setup_address(city, state, zip_code)
     raise 'invalid login' unless logged?
+
     @driver.navigate.to 'https://www.tutorselect.com/portal/myaccount.aspx'
+
+    @driver.find_element(:id, EDIT_ADDRESS_PROFILE_ID).click
+
+    city_textbox = @driver.find_element(:id, CITY_TEXTBOX_ID)
+    city_textbox.clear
+    city_textbox.send_keys(city)
+
+    zip_textbox = @driver.find_element(:id, ZIPCODE_TEXTBOX_ID)
+    zip_textbox.clear
+    zip_textbox.send_keys(zip_code)
+
+    dropDownMenu = @driver.find_element(:id, STATES_SELECT_ID)
+    option       = Selenium::WebDriver::Support::Select.new(dropDownMenu)
+    option.select_by(:value, state)
+
+    @driver.find_element(:id, ADDRESS_PROFILE_SUBMIT_ID).click
   end
 end
--- spec/selenium_directives_spec.rb
+++ spec/selenium_directives_spec.rb
@@ -32,22 +32,58 @@ describe SeleniumDirectives do
   end
 
   describe ".setup_address" do
+    before(:each) do
+      @navigate_mock = double('navigateElement')
+      allow(@navigate_mock).to receive(:to)
+
+      @span_mock = double('spanElement')
+      allow(@span_mock).to receive(:text).and_return('Hello FakeUser!')
+
+      @element_mock = double('element')
+
+      #Used as textbox
+      allow(@element_mock).to receive(:clear)
+      allow(@element_mock).to receive(:click)
+      allow(@element_mock).to receive(:send_keys)
+
+      select_item_mock = double('SelectItem')
+      allow(select_item_mock).to receive(:click)
+      allow(select_item_mock).to receive(:selected?).and_return(:false)
+
+      #Used as combobox. This stub methods are required for Selenium::WebDriver::Support::Select
+      allow(@element_mock).to receive(:tag_name).and_return('select')
+      allow(@element_mock).to receive(:attribute).with(:multiple)
+      allow(@element_mock).to receive(:find_elements).with(:xpath, an_instance_of(String)).and_return([select_item_mock])
+
+
+      @driver_mock = double(Selenium::WebDriver::Driver)
+      allow(@driver_mock).to receive(:navigate).and_return(@navigate_mock)
+      allow(@driver_mock).to receive(:find_element).with(:class, an_instance_of(String)).and_return(@span_mock)
+      allow(@driver_mock).to receive(:find_element).with(:id,    an_instance_of(String)).and_return(@element_mock)
+    end
+
     context '.logged? is true' do
       it "navigate to profile edit url" do
-        navigate_mock = double('navigate')
-        allow(navigate_mock).to receive(:to)
-
-        text_mock = double('spanElement')
-        allow(text_mock).to receive(:text).and_return('Hello FakeUser!')
-
-        driver_mock = double(Selenium::WebDriver::Driver)
-        allow(driver_mock).to receive(:find_element).with(:class, an_instance_of(String)).and_return(text_mock)
-        allow(driver_mock).to receive(:navigate).and_return(navigate_mock)
-
-        sut = SeleniumDirectives.new driver_mock
+        sut = SeleniumDirectives.new @driver_mock
 
         sut.setup_address('city', 'state', 'zip_code')
-        expect(navigate_mock).to have_received(:to).with('https://www.tutorselect.com/portal/myaccount.aspx')
+        expect(@navigate_mock).to have_received(:to).with('https://www.tutorselect.com/portal/myaccount.aspx')
+      end
+
+      it "form filled and posted" do
+        sut = SeleniumDirectives.new @driver_mock
+
+        sut.setup_address('Fake City', 'FS', '00100')
+
+        expect(@element_mock).to have_received(:clear).twice
+
+        expect(@element_mock).to have_received(:send_keys).with('Fake City').once
+        expect(@element_mock).to have_received(:send_keys).with('00100').once
+
+        expect(@element_mock).to have_received(:find_elements).with(:xpath, /FS/).once
+
+        #One time for the edit button and other for submit
+        expect(@element_mock).to have_received(:click).twice
       end
     end
 
