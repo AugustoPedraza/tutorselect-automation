309aabc427720a1f4fe00b01d445d666e8858c20
Augusto Pedraza <augusto.pedraza08@gmail.com>
2014-04-13 14:28:57 -0300

    Refactoring added

--- lib/opportunities_analyzer.rb
+++ lib/opportunities_analyzer.rb
@@ -1,16 +1,29 @@
 class OpportunitiesAnalyzer
-  @@opportunities = {}
 
-  def self.add(data)
-    data[:subject].slice!(/\s+near\s+/)
-    key      = data[:subject].gsub(/\s+/, '_').gsub(/\d{5}/, '').downcase
-    zip_code = data[:subject].scan(/\d{5}/).first
-
-    opportunity_id = "%s_%s_%s" % [data[:user_id], zip_code, data[:date]]
-    @@opportunities[key.to_sym] = {opportunity_id => 1}
+  def initialize
+    @grouped_subjects = {}
   end
 
-  def self.all
-    @@opportunities
+  def add(data)
+    data[:subject].slice!(/\s+near\s+/)
+    subject_id      = data[:subject].gsub(/\s+/, '_').gsub(/\d{5}/, '').downcase.to_sym
+    zip_code        = data[:subject].scan(/\d{5}/).first
+    opportunity_id  = "%s_%s_%s" % [data[:user_id], zip_code, data[:date]]
+
+    if @grouped_subjects.include? subject_id
+      grouped_opportunities = @grouped_subjects[subject_id]
+
+      if grouped_opportunities.include? opportunity_id
+        grouped_opportunities[opportunity_id] = grouped_opportunities[opportunity_id] + 1
+      else
+        grouped_opportunities[opportunity_id] = 1
+      end
+    else
+      @grouped_subjects[subject_id] = { opportunity_id => 1 }
+    end
+  end
+
+  def all
+    @grouped_subjects
   end
 end
--- spec/opportunities_analyzer_spec.rb
+++ spec/opportunities_analyzer_spec.rb
@@ -6,9 +6,21 @@ describe OpportunitiesAnalyzer do
       context 'simple subject' do
         it "add new opportunity" do
           data = { username: 'Kerry T.', user_id: '11211', subject: 'Dyslexia near 11111', date: '04/11' }
-          OpportunitiesAnalyzer.add(data)
-          expect(OpportunitiesAnalyzer.all).to eql({ dyslexia: { "11211_11111_04/11" => 1 } })
+          sut = OpportunitiesAnalyzer.new
+          sut.add(data)
+          expect(sut.all).to eql({ dyslexia: { "11211_11111_04/11" => 1 } })
         end
+      end
+    end
+
+    context 'same opportunity loaded twice' do
+      it "update opportunity counter" do
+        data = { username: 'Kerry T.', user_id: '11211', subject: 'Dyslexia near 11111', date: '04/11' }
+        sut = OpportunitiesAnalyzer.new
+        sut.add(data)
+        sut.add(data)
+
+        expect(sut.all).to eql({ dyslexia: { "11211_11111_04/11" => 2 } })
       end
     end
   end
