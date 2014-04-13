3f0578412377a73cd4dd8ad7ce267f4ff5e0cc38
Augusto Pedraza <augusto.pedraza08@gmail.com>
2014-04-13 15:42:29 -0300

    New spec added

--- spec/opportunities_analyzer_spec.rb
+++ spec/opportunities_analyzer_spec.rb
@@ -10,17 +10,29 @@ describe OpportunitiesAnalyzer do
           sut.add(data)
           expect(sut.all).to eql({ dyslexia: { "11211_11111_04/11" => 1 } })
         end
+
+        it "update opportunity counter twice" do
+          data = { username: 'Kerry T.', user_id: '11211', subject: 'Dyslexia near 11111', date: '04/11' }
+          sut = OpportunitiesAnalyzer.new
+          sut.add(data)
+          sut.add(data)
+
+          expect(sut.all).to eql({ dyslexia: { "11211_11111_04/11" => 2 } })
+        end
       end
-    end
 
-    context 'same opportunity loaded twice' do
-      it "update opportunity counter" do
-        data = { username: 'Kerry T.', user_id: '11211', subject: 'Dyslexia near 11111', date: '04/11' }
-        sut = OpportunitiesAnalyzer.new
-        sut.add(data)
-        sut.add(data)
+      context "various subjects" do
+        it "add two differents opportunities" do
+          dyslexia_op = { username: 'Kerry T.',  user_id: '11211', subject: 'Dyslexia near 11111', date: '04/11' }
+          french_op   = { username: 'August P.', user_id: '20050', subject: 'French near 99099',  date: '05/11' }
+          sut = OpportunitiesAnalyzer.new
+          sut.add(dyslexia_op)
+          sut.add(french_op)
 
-        expect(sut.all).to eql({ dyslexia: { "11211_11111_04/11" => 2 } })
+          expect(sut.all).to eql({
+            dyslexia: { "11211_11111_04/11" => 1 },
+            french:   { "20050_99099_05/11" => 1 } })
+        end
       end
     end
   end
