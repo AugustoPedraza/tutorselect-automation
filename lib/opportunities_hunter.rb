09f5e5f07f270c817b868b9542f11a07b0969231
Augusto Pedraza <augusto.pedraza08@gmail.com>
2014-04-26 10:45:53 -0300

    Redefined class

--- lib/opportunities_hunter.rb
+++ lib/opportunities_hunter.rb
@@ -1,21 +1,20 @@
 class OpportunitiesHunter
 
-  attr_reader :subjects, :location
+  attr_reader :account_email, :account_password
 
   AREA_REQUEST_TABLE_ID     = 'Main_div_opp'
   CUSTOM_MESSAGE            = "fake message"
 
-  def initialize(selenium_directives, subject, location)
-    @selenium_directives = selenium_directives
-    @subjects ||= []
-    @subjects << subject
-    @location = location
+  def initialize(selenium_directives, email, password)
+    @selenium_directives  = selenium_directives
+    @account_email        = email
+    @account_password     = password
   end
 
-  def hunter_all
-    @selenium_directives.setup_address(@location.city, @location.state.to_s, @location.zip_code.to_s)
+  def hunter_all(location, subject)
+    @selenium_directives.setup_address(location.city, location.state.to_s, location.zip_code.to_s)
     @selenium_directives.get_table_data_by_container_id(AREA_REQUEST_TABLE_ID).each do |opportunity|
-      user_id  =opportunity[:user_profile].split('/').last.to_i
+      user_id  = opportunity[:user_profile].split('/').last.to_i
       @selenium_directives.send_message(user_id, CUSTOM_MESSAGE)
     end
   end
--- spec/opportunities_hunter_spec.rb
+++ spec/opportunities_hunter_spec.rb
@@ -2,20 +2,19 @@ require 'spec_helper'
 
 describe OpportunitiesHunter do
   describe "initialization" do
-    context "with a single subject a location and a valid selenium directive instance" do
+    context "with valid credentials" do
       it "create a valid object" do
-        fake_location = Location.new({ address: "fake address", state: :NY, zip_code: "10001" })
         fake_selenium_directives = double(SeleniumDirectives)
 
-        sut = OpportunitiesHunter.new(fake_selenium_directives, :spanish, fake_location)
-        expect(sut.subjects).to match_array ([:spanish])
-        expect(sut.location).to equal(fake_location)
+        sut = OpportunitiesHunter.new(fake_selenium_directives, "fake@email.com", "fake_password")
+        expect(sut.account_email).to eql("fake@email.com")
+        expect(sut.account_password).to eql("fake_password")
       end
     end
   end
 
   describe ".hunter_all" do
-    context "receive a selenium directives instance already logged" do
+    context "selenium directives instance already logged" do
       context "5 opportunities founded" do
         it "send 5 messages" do
           selenium_directives_mock = double('selenium_directives')
@@ -36,10 +35,10 @@ describe OpportunitiesHunter do
           allow(selenium_directives_mock).to receive(:get_table_data_by_container_id).with(kind_of(String)).and_return(fake_opportunities).ordered
           allow(selenium_directives_mock).to receive(:send_message).with(kind_of(Integer), kind_of(String)).ordered
 
-          dummy_location  = Location.new({ city: "fake city", state: :NY, zip_code: 11011 })
-          sut = OpportunitiesHunter.new(selenium_directives_mock, "fake subject", dummy_location)
+          fake_location  = Location.new({ city: "fake city", state: :NY, zip_code: 11011 })
+          sut = OpportunitiesHunter.new(selenium_directives_mock, "dummy@email.com", "dummy_password")
 
-          sut.hunter_all
+          sut.hunter_all(fake_location, "fake subject")
 
           expect(selenium_directives_mock).to have_received(:setup_address).once.with("fake city", "NY", "11011")
 
@@ -47,6 +46,27 @@ describe OpportunitiesHunter do
             expect(selenium_directives_mock).to have_received(:send_message).once.with(fake_id, 'fake message')
           end
         end
+      end
+    end
+
+    context "receive a selenium directives instance not logged" do
+      it "try login" do
+        pending("something else getting finished")
+        this_should_not_get_executed
+        # selenium_directives_mock = double('selenium_directives')
+
+        # allow(selenium_directives_mock).to receive(:logged?).and_return(false)
+        # allow(selenium_directives_mock).to receive(:login).with(kind_of(String), kind_of(String))
+        # allow(selenium_directives_mock).to receive(:setup_address).with(kind_of(String), kind_of(String), kind_of(String))
+        # allow(selenium_directives_mock).to receive(:get_table_data_by_container_id).with(kind_of(String)).and_return([])
+        # allow(selenium_directives_mock).to receive(:send_message).with(kind_of(Integer), kind_of(String))
+
+        # dummy_location  = Location.new({ city: "fake city", state: :NY, zip_code: 11011 })
+        # sut = OpportunitiesHunter.new(selenium_directives_mock, "fake subject", dummy_location)
+
+        # sut.hunter_all
+
+        # expect(selenium_directives_mock).to have_received(:login).once.with("fake city", "NY", "11011")
       end
     end
   end
